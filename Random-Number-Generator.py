05d0af5ee302fa939ed8e40b2b35a615204811be
Atheidon <93974381+Atheidon@users.noreply.github.com>
2021-11-09 09:37:45 +0100

    Add files via upload

---
+++ Random-Number-Generator.py
@@ -0,0 +1,7 @@
+import random
+lowest_number = int(input("Give the lowest possible number: "))
+highest_number = int(input("Give the highest possible number: "))
+amount = int(input("Give the amount of random numbers: "))
+for i in range(amount):
+    print(random.randrange(lowest_number, highest_number + 1))
+input("press enter to exit")
