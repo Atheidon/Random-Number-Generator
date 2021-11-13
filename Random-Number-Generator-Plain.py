2284ad8fd7a9a8576ba85c0def5be426c9aa178e
Atheidon <93974381+Atheidon@users.noreply.github.com>
2021-11-13 12:09:02 +0100

    Update and rename Random-Number-Generator.py to Random-Number-Generator-Plain.py

--- Random-Number-Generator-Plain.py
+++ Random-Number-Generator-Plain.py
@@ -2,6 +2,4 @@ import random
 lowest_number = int(input("Give the lowest possible number: "))
 highest_number = int(input("Give the highest possible number: "))
 amount = int(input("Give the amount of random numbers: "))
-for i in range(amount):
-    print(random.randrange(lowest_number, highest_number + 1))
-input("press enter to exit")
+for i in range(amount): print(random.randrange(lowest_number, highest_number + 1))
