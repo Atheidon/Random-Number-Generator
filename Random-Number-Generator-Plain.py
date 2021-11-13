8a48cd10b507cfb897d828b133f1d89eb6fdaf82
Atheidon <93974381+Atheidon@users.noreply.github.com>
2021-11-13 12:20:22 +0100

    Delete Random-Number-Generator-Plain.py

--- Random-Number-Generator-Plain.py
+++
@@ -1,5 +0,0 @@
-import random
-lowest_number = int(input("Give the lowest possible number: "))
-highest_number = int(input("Give the highest possible number: "))
-amount = int(input("Give the amount of random numbers: "))
-for i in range(amount): print(random.randrange(lowest_number, highest_number + 1))
